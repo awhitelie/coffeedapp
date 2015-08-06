09ebdf10079c2f838bbfce97f3d74442f10027cc
awhitelie <czegel@gmail.com>
2015-08-06 07:54:16 -0400

    Importing template

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+import core.views as coreviews
+
+urlpatterns = [
+    url(r'^$', coreviews.LandingView.as_view()),
+]
