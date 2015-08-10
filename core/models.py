eef1f68cd002b06656eb9c01546da513342b2ba4
awhitelie <czegel@gmail.com>
2015-08-10 16:14:41 -0400

    building list and detail views

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Location(models.Model):
@@ -8,3 +9,6 @@ class Location(models.Model):
 
     def __unicode__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(viewname='location_list', args=[self.id])
